efcadf79c543dc4dad2000600bc7207fdc3d1e78
schmidtb <benediktschmidt89@googlemail.com>
2013-12-06 16:07:29 +0100

    nix geändert

--- JavaEEUebung/src/javaee/dev/dao/AbstractDAO.java
+++ JavaEEUebung/src/javaee/dev/dao/AbstractDAO.java
@@ -8,10 +8,9 @@ import javaee.dev.model.HibernateUtil;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
-
 public abstract class AbstractDAO<T extends Object> {
 
-	public void delete(T sight){
+	public void delete(T sight) {
 		Session session = null;
 
 		try {
@@ -22,18 +21,19 @@ public abstract class AbstractDAO<T extends Object> {
 				session.flush();
 			}
 			session.getTransaction().commit();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			HibernateUtil.closeSession(session);
 		}
 	}
-	
+
 	/**
-	 * Sucht eine Sehenswürdigkeit anhand ihres Primärschlüssels aus der Datenbank.
-	 * @param id Der Primärschlüssel der zu suchenden Sehenswürdigkeit.
+	 * Sucht eine Sehenswürdigkeit anhand ihres Primärschlüssels aus der
+	 * Datenbank.
+	 * 
+	 * @param id
+	 *            Der Primärschlüssel der zu suchenden Sehenswürdigkeit.
 	 * @return Die gefundene Sehenswürdigkeit oder null.
 	 */
 	public T get(int id) {
@@ -42,23 +42,22 @@ public abstract class AbstractDAO<T extends Object> {
 		try {
 			session = HibernateUtil.getSessionFactory().getCurrentSession();
 			session.beginTransaction();
-			T sight = (T)session.get(getHibernateClass(), id);
+			T sight = (T) session.get(getHibernateClass(), id);
 			session.getTransaction().commit();
 			return sight;
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		}
-		finally {
+		} finally {
 			HibernateUtil.closeSession(session);
 		}
 	}
-	
+
 	protected abstract Class getHibernateClass();
 
 	/**
 	 * Gibt alle Sehenswürdigkeiten als Liste aus der Datenbank zurück.
+	 * 
 	 * @return Alle Sehenswürdigkeiten.
 	 */
 	public List<T> getList() {
@@ -71,22 +70,23 @@ public abstract class AbstractDAO<T extends Object> {
 			session.beginTransaction();
 
 			Query q = session.createQuery(queryString);
-			resultList = (ArrayList<T>)q.list();
+			resultList = (ArrayList<T>) q.list();
 			session.getTransaction().commit();
 			return resultList;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		}
-		finally {
+		} finally {
 			HibernateUtil.closeSession(session);
 		}
 	}
-	
+
 	/**
-	 * Sucht alle Sehenswürdigkeiten aus der Datenbank, deren Name dem Suchstring entspricht.
-	 * @param searchString Der Suchstring.
+	 * Sucht alle Sehenswürdigkeiten aus der Datenbank, deren Name dem
+	 * Suchstring entspricht.
+	 * 
+	 * @param searchString
+	 *            Der Suchstring.
 	 * @return Alle passenden Sehenswürdigkeiten.
 	 */
 	public List<T> getList(String searchString) {
@@ -100,23 +100,24 @@ public abstract class AbstractDAO<T extends Object> {
 
 			Query q = session.createQuery(queryString);
 			q.setString(0, "%" + searchString + "%");
-			resultList = (ArrayList<T>)q.list();
+			resultList = (ArrayList<T>) q.list();
 			session.getTransaction().commit();
 			return resultList;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		}
-		finally {
+		} finally {
 			HibernateUtil.closeSession(session);
 		}
 	}
-	
+
 	/**
-	 * Speichert eine Sehenswürdigkeit in der Datenbank. Diese Methode erkennt selbstständig, ob ein Objekt neu in die
-	 * Datenbank eingefügt oder dort aktualisiert werden muss.
-	 * @param sight Die zu speichernde Sehenswürdigkeit.
+	 * Speichert eine Sehenswürdigkeit in der Datenbank. Diese Methode erkennt
+	 * selbstständig, ob ein Objekt neu in die Datenbank eingefügt oder dort
+	 * aktualisiert werden muss.
+	 * 
+	 * @param sight
+	 *            Die zu speichernde Sehenswürdigkeit.
 	 */
 	public void save(T sight) {
 		Session session = null;
@@ -127,11 +128,9 @@ public abstract class AbstractDAO<T extends Object> {
 			session.saveOrUpdate(sight);
 			session.flush();
 			session.getTransaction().commit();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			HibernateUtil.closeSession(session);
 		}
 	}
