4d073bb64e9b8b475019734996305e46abf14fc9
schmidtb <benediktschmidt89@googlemail.com>
2014-01-10 15:43:57 +0100

    AbstractDAO verfeinert

--- JavaEEUebung/src/javaee/dev/dao/AbstractDAO.java
+++ JavaEEUebung/src/javaee/dev/dao/AbstractDAO.java
@@ -5,19 +5,22 @@ import java.util.List;
 
 import javaee.dev.model.HibernateUtil;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 public abstract class AbstractDAO<T extends Object> {
 
-	public void delete(T sight) {
+	public void delete(T object) {
 		Session session = null;
 
 		try {
 			session = HibernateUtil.getSessionFactory().getCurrentSession();
 			session.beginTransaction();
-			if (sight != null) {
-				session.delete(sight);
+			if (object != null) {
+				session.delete(object);
 				session.flush();
 			}
 			session.getTransaction().commit();
@@ -53,7 +56,7 @@ public abstract class AbstractDAO<T extends Object> {
 		}
 	}
 
-	protected abstract Class getHibernateClass();
+	protected abstract Class<T> getHibernateClass();
 
 	/**
 	 * Gibt alle Sehenswürdigkeiten als Liste aus der Datenbank zurück.
@@ -62,15 +65,12 @@ public abstract class AbstractDAO<T extends Object> {
 	 */
 	public List<T> getList() {
 		Session session = null;
-		List<T> resultList;
-		String queryString = "SELECT s FROM SightHibernateImpl s";
 
 		try {
 			session = HibernateUtil.getSessionFactory().getCurrentSession();
 			session.beginTransaction();
-
-			Query q = session.createQuery(queryString);
-			resultList = (ArrayList<T>) q.list();
+			
+			List<T> resultList = (ArrayList<T>) session.createCriteria(getHibernateClass()).list();
 			session.getTransaction().commit();
 			return resultList;
 		} catch (Exception e) {
@@ -89,18 +89,18 @@ public abstract class AbstractDAO<T extends Object> {
 	 *            Der Suchstring.
 	 * @return Alle passenden Sehenswürdigkeiten.
 	 */
-	public List<T> getList(String searchString) {
+	public List<T> getList(Criterion...criterions) {
 		Session session = null;
-		List<T> resultList;
-		String queryString = "SELECT s FROM SightHibernateImpl s where s.sightName like ?";
 
 		try {
 			session = HibernateUtil.getSessionFactory().getCurrentSession();
 			session.beginTransaction();
 
-			Query q = session.createQuery(queryString);
-			q.setString(0, "%" + searchString + "%");
-			resultList = (ArrayList<T>) q.list();
+			Criteria criteria = session.createCriteria(getHibernateClass());
+			for(Criterion nextCriterion : criterions){
+				criteria.add(nextCriterion);
+			}
+			List<T>resultList = (ArrayList<T>) criteria.list();
 			session.getTransaction().commit();
 			return resultList;
 		} catch (Exception e) {
